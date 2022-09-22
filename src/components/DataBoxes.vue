5f320603468f1da0a44409595f4170bd4bbf9e96
Kwade Jeffrey <kwadejeffrey@gmail.com>
2022-09-22 10:43:45 +0000

    second commit

--- src/components/DataBoxes.vue
+++ src/components/DataBoxes.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid md:grid-cols-2 gap-4">
     <!--grid 1-->
-    <div class="shadow-md bg-blue-200 p-10 text-center rounded">
+    <div class="shadow-md bg-blue-200 hover:bg-blue-500 p-10 text-center rounded">
       <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
       <div class="text-2xl mb-4">
         <span class="font-semibold"> New:</span>
